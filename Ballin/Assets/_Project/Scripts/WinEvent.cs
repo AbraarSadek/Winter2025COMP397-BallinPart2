bf0d38b365edf870ca5ffde62db03f5f84988568
Abraar Sadek <abraarsadek@gmail.com>
2025-04-05 23:18:16 -0400

    Achievement System - 04/05/2025
    Added:
    
              - Achievement System

--- Ballin/Assets/_Project/Scripts/WinEvent.cs
+++ Ballin/Assets/_Project/Scripts/WinEvent.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class WinEvent : MonoBehaviour
-{
-    private void OnTriggerEnter(Collider other)
-    {
+public class WinEvent : MonoBehaviour {
 
-        if (other.CompareTag("Player"))
-        {
+    [SerializeField] private PlayerHealthController playerHealthController;
+    [SerializeField] private CoinCollectionController coinCollectionController;
+    [SerializeField] private TimerController timerController;
+
+    public static class GameData {
+        public static int remainingHealthBars;
+        public static int coinsCollected;
+        public static float remainingTime;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        GameData.remainingHealthBars = playerHealthController.remainingHealthBars;
+        GameData.coinsCollected = coinCollectionController.coinsCollected;
+        GameData.remainingTime = timerController.timeRemaining;
+
+        if (other.CompareTag("Player")) {
 
             SceneManager.LoadScene("WinScene");
 
         }
 
-    } 
+    }
+
 }
--- Ballin/Assets/_Project/Scripts/endScoreManager.cs
+++ Ballin/Assets/_Project/Scripts/endScoreManager.cs
@@ -1,23 +1,43 @@
 using UnityEngine;
 using TMPro;
+using System;
+using static WinEvent;
 
-public class EndScoreManager : MonoBehaviour
-{
-    [SerializeField] private TMP_Text scoreDisplay;
-    [SerializeField] private TMP_Text coinDisplay;
+public class EndScoreManager : MonoBehaviour {
 
-    [SerializeField] private FloatSO score;
-    [SerializeField] private FloatSO cointCount;
-    void Start()
-    {
-        scoreDisplay.text = score.Value.ToString();
-        coinDisplay.text = cointCount.Value.ToString();
+    [SerializeField] private GameObject starOne;
+    [SerializeField] private GameObject starTwo;
+    [SerializeField] private GameObject starThree;
+
+    int remainingHealthBars;
+    int coinsCollected;
+    float remainingTime;
+
+    //References to Other Scripts
+    private PlayerHealthController playerHealthController;
+    private CoinCollectionController coinCollectionController;
+    private TimerController timerController;
+
+    void Awake() {
+
+        remainingHealthBars = WinEvent.GameData.remainingHealthBars; //Set The Remaining Health Bars To The Value Stored In The Game Data
+        coinsCollected = WinEvent.GameData.coinsCollected; //Set The Coins Collected To The Value Stored In The Game Data
+        remainingTime = WinEvent.GameData.remainingTime; //Set The Remaining Time To The Value Stored In The Game Data
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-    
+    void Update() {
+        AwardStars();
     }
+
+    private void AwardStars() {
+
+        //If-Statement - That Will Award The Player Stars If They Have Met The Requirements
+        if (remainingHealthBars == 3) { starOne.SetActive(true); /*Set Game Object To Active*/ }
+        if (remainingTime >= 60f) { starTwo.SetActive(true); /*Set Game Object To Active*/ }
+        if (coinsCollected == 3) { starThree.SetActive(true); /*Set Game Object To Active*/ }
+
+    }
+
 }
